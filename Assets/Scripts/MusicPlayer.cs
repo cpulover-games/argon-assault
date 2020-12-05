c39a3d32c1824b83d42382d13d46b8fa7971d1b0
Hieu Ngo <justacpulover@gmail.com>
2020-12-05 16:32:17 +0100

    Add splash screen

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(LoadFirstLevel), 2.5f);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
