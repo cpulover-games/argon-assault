cd46fca03542c725a3c84de8754c1448517df145
Hieu Ngo <justacpulover@gmail.com>
2020-12-07 21:21:47 +0100

    Add mesh collider for enemies in runtime

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,13 +7,32 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupCollider();
+    }
+
+    private void SetupCollider()
+    {
+        MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+
+        // create new in case collider not included in pre-built asset
+        if (meshCollider == null)
+            meshCollider = gameObject.AddComponent<MeshCollider>();
+        // disable trigger to interact with particles
+        meshCollider.isTrigger = false;
+        // enable convex to improve performance
+        meshCollider.convex = true;
+        // add mesh from Mesh Filter component to new Collinder component
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        if (mesh != null)
+        {
+            meshCollider.sharedMesh = mesh;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnParticleCollision(GameObject other)
